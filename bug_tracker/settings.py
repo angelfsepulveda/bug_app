70fafc0b3a9cb4c0c32bb8c687e20c12ceaacdae
1430382 <1430382@upv.edu.mx>
2021-04-20 14:52:31 -0500

    hide secret key

--- bug_tracker/settings.py
+++ bug_tracker/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from data_project import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f#m+@)k&w+$xl2uc686um$g)v5*-spw9*o8co_0)5)o!$lq=gu'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
